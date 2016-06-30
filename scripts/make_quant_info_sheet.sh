d436a97b23e1080db3dd88c167fd16072aa0a9ca
Liz Vincent <liz.vincent92@gmail.com>
2016-06-30 11:23:50 -0400

    Create make_quant_info_sheet.sh
    This is the code I used to create the quant_files.txt for Chakravarti_HD_02.  Currently needs to be run as 
    ./make_quant_info_sheet.sh > quant_files.txt
    from command line, script itself will only output the file but not save it.

---
+++ scripts/make_quant_info_sheet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROJECT_ROOT="$HOME/work/seq/shared/Chakravarti_HD_02"
+ALIGN_ROOT="$PROJECT_ROOT/data/bam"
+
+echo "sample_id group_label"
+
+for dir in $ALIGN_ROOT/*;
+  do
+  	CELLID=`basename $dir`
+ 		f="$CELLID.bam"
+    echo "$PROJECT_ROOT/data/quants/$CELLID/abundances.cxb      $CELLID"
+  done;
